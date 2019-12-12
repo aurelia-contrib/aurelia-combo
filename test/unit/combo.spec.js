3cf0bddf06b05294ee3b27fb60265563a37c73b2
Chunpeng Huo <huocp@me.com>
2019-12-13 10:08:23 +1100

    chore: upgrade packages, cleanup test code

--- test/unit/combo.spec.js
+++ test/unit/combo.spec.js
@@ -6,31 +6,31 @@ import {inlineView} from 'aurelia-framework';
 
 configure(); // monkey patch attached/detached
 
-var modifierMap = {
-  16:'shiftKey',
-  18:'altKey',
-  17:'ctrlKey',
-  91:'metaKey'
+const modifierMap = {
+  16: 'shiftKey',
+  18: 'altKey',
+  17: 'ctrlKey',
+  91: 'metaKey'
 };
 
-var KEYS = {
+const KEYS = {
   '⇧': 16, shift: 16,
   '⌥': 18, alt: 18, option: 18,
   '⌃': 17, ctrl: 17, control: 17,
   '⌘': 91, command: 91
 };
 
-function keydown(code, modifiers){
+function keydown(code, modifiers) {
   var event = document.createEvent('Event');
   event.initEvent('keydown', true, true);
   event.keyCode = code;
   if (modifiers && modifiers.length > 0) {
-    for(const i in modifiers) event[modifierMap[modifiers[i]]] = true;
+    for (const i in modifiers) event[modifierMap[modifiers[i]]] = true;
   }
   document.dispatchEvent(event);
 }
 
-function keyup(code){
+function keyup(code) {
   var event = document.createEvent('Event');
   event.initEvent('keyup', true, true);
   event.keyCode = code;
