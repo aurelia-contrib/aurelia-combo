e1393bf3b2f1ca8d291e75c475a1bf8b75105d20
Zach Hall <zach@zachhall.org>
2020-04-13 19:45:35 -0400

    fix: update TypeScript definition to match the new argument

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
-export declare function combo(...shortcuts: string[]): any;
+export declare function combo(...shortcuts: (string|boolean)[]): any;
 export declare function configure(frameworkConfig?: any): void;
